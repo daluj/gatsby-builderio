48f454e8962c05a4e82c90b1fdc038b083ea0921
david <davidluengo8@gmail.com>
2020-11-12 17:48:03 +0100

    environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 const config = require('./src/config');
 module.exports = {
   pathPrefix: "/gatsby-starter-builder",
